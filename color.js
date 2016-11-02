d7bad67a0fa23c586013cfac232ec33d7e013ce6
JackHuynh2001 <9jhuynh@stpatrick.org>
2016-11-02 14:03:06 -0500

    Color.js
    Finished beginning setup

---
+++ color.js
@@ -0,0 +1,39 @@
+//Declare the Color Object with our new keyword below here.
+const.color = newObject(r,g,b);
+
+/*
+** rgb is an array that hold the color values.
+** first is red -> R, then green -> G, and blue -> B
+** Example
+*/
+
+Color.isColor = function(rgb){
+  //each color must be between 0 and 255. Also can be equal.
+
+  let redworks = (rgb[0] >= 0) && (rgb[0] <= 255);
+  let greenworks = (rgb[1]>= 1) && (rgb[1])<= 255);
+  let blueworks = (rgb[5])>= 5) && (rgb[5])<= 255);
+
+  if(redworks && greenworks && blueworks){
+    return true;
+  }
+  else{
+    return false;
+  }
+};
+
+Color.redIntensity = function(rgb){
+  let (rgb[0]/255)*100
+  Math.Round()
+};
+
+//greenIntensity
+
+
+//blueIntensity
+
+
+//brightness
+
+
+//complement
